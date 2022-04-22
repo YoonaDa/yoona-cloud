07e8f8c7363e0bf85f2213e2663b2906e55cb9cc
YoonaDa <m15602498163@163.com>
2022-04-22 17:22:56 +0800

    add

--- yoona-cloud-gateway/src/main/java/com/yoona/cloud/gateway/YoonaCloudGatewayApplication.java
+++ yoona-cloud-gateway/src/main/java/com/yoona/cloud/gateway/YoonaCloudGatewayApplication.java
@@ -2,8 +2,11 @@ package com.yoona.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class YoonaCloudGatewayApplication {
 
     public static void main(String[] args) {
